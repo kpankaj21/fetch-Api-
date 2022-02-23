3b615d260d3252f2ba1d80f86205352216095170
kpankaj21 <kamaliyapankaj007@gmail.com>
2022-02-23 18:45:38 +0530

    show image using GET mathod

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { useEffect, useState } from "react";
+import Grid  from "@mui/material";
 
-function App() {
+const App = () => {
+  const [image, setImage] = useState([]);
+
+  //get Value
+
+  //  fetch("https://jsonplaceholder.typicode.com/users").then((data) => {
+  //   // console.log(data);
+  //   return data.json()
+  // }).then((actualdata) => {
+  //   console.log("actualData",actualdata[0].address.city);
+  //   setImage(actualdata[0].address.city)
+
+  // }
+  // ).catch((error) => {
+  //   console.log(error);
+  // });
+
+  //image
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/photos")
+      .then((data) => {
+        return data.json();
+      })
+      .then((actualData) => {
+        console.log("ActualDATA", actualData);
+        // console.log("ActualDATA", actualData[0].url);
+        setImage(actualData);
+        console.log("image", image);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {image} */}
+     {image.map((item) => {
+        return( 
+        <div className="item">
+      <img style={{width:"200px"}} src={item.url} />
+     </div>
+     )})}
+
     </div>
-  );
+  )   
 }
 
 export default App;
